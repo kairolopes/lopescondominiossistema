36892048eea04b989ba9d16ec0a2b0556eb025a5
kairolopes <151247602+kairolopes@users.noreply.github.com>
2026-01-31 01:05:54 -0300

    Set default tokens in env configuration
    Updated default configuration values for ZAPI and Superlogica tokens.

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,19 +5,19 @@ dotenv.config();
 export const config = {
   port: process.env.PORT || 3000,
   zapi: {
-    instanceId: process.env.ZAPI_INSTANCE_ID || '',
-    token: process.env.ZAPI_TOKEN || '',
-    securityToken: process.env.ZAPI_SECURITY_TOKEN || '',
+    instanceId: process.env.ZAPI_INSTANCE_ID || '3EDDC00C4442415A1099DEFCC216B74C',
+    token: process.env.ZAPI_TOKEN || '6909F71A3D29D570F0A8C65C',
+    securityToken: process.env.ZAPI_SECURITY_TOKEN || 'Ff94d05bcd8b546afb957fc52d8e33ebaS',
   },
   superlogica: {
-    appToken: process.env.SUPERLOGICA_APP_TOKEN || '',
-    accessToken: process.env.SUPERLOGICA_ACCESS_TOKEN || '',
+    appToken: process.env.SUPERLOGICA_APP_TOKEN || '47f3393f-0d2c-4928-9ee8-ccda82de50b8',
+    accessToken: process.env.SUPERLOGICA_ACCESS_TOKEN || '0bae402b-1e2b-4145-ab04-a726f37a2870',
     url: process.env.SUPERLOGICA_URL || 'https://api.superlogica.net/v2/condor',
   },
   openai: {
     // Deprecated
   },
   gemini: {
-    apiKey: process.env.GEMINI_API_KEY || '',
+    apiKey: process.env.GEMINI_API_KEY || 'AIzaSyA9ZA-a_Tas3t-v3RTMX_9FdkTGRcIlTbM',
   }
 };
